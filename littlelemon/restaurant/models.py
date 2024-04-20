dd679a0ada5522d2f36e7e5a3a26f6cb042e2594
Andres Echeverry <andresecheverry@Andress-MacBook-Pro-2.local>
2024-04-20 20:57:02 +0100

    models and serializers added

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,17 +1,16 @@
 import datetime
-
 from django.db import models
 
 # Create your models here.
 
 class Booking(models.Model):
-    ID = models.IntegerField(max_length=11)
-    Name = models.CharField(max_length=255)
-    No_of_guests = models.IntegerField(max_length=6)
-    BookingDate = datetime.date
+    booking_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = datetime.date
 
 class Menu(models.Model):
-    ID = models.IntegerField(max_length=11)
-    Title = models.CharField(max_length=255)
-    Price = models.DecimalField(max_digits=10, decimal_places=2)
-    Inventory = models.IntegerField(max_length=5)
+    menu_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,15 @@
+import rest_framework.generics
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import Menu
+from .serializers import MenuSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+class MenuItemView(rest_framework.generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+#class SingleMenuItemView(rest_framework.generics.RetrieveUpdateDestroyAPIView, rest_framework.generics.DestroyAPIView)
